b43adb24c8efe5467166270dd3026a2fccb1780f
Rajat Gupta <rajatgupta0889@gmail.com>
2014-12-31 00:28:24 +0530

    Update Design
    Add Expand List

--- OnlineDating/src/online/daing/onlinedating/HomeFragment.java
+++ OnlineDating/src/online/daing/onlinedating/HomeFragment.java
@@ -1,21 +1,51 @@
 package online.daing.onlinedating;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import online.dating.onlinedating.adapter.UserInfoExpandListAdapter;
+import online.dating.onlinedating.model.UserDetailItem;
+import online.dating.onlinedating.model.UserInfoItem;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
 
 public class HomeFragment extends Fragment {
-	
-	public HomeFragment(){}
-	
+
+	public HomeFragment() {
+	}
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-         
-        return rootView;
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_home, container,
+				false);
+		ExpandableListView userList = (ExpandableListView) rootView
+				.findViewById(R.id.expandableUserInfoListView);
+
+		ArrayList<UserInfoItem> userInfoItems = new ArrayList<UserInfoItem>();
+		userInfoItems.add(new UserInfoItem("How tall are you",
+				R.drawable.ic_user_height));
+		userInfoItems.add(new UserInfoItem("Your Passion",
+				R.drawable.ic_user_passion));
+		userInfoItems.add(new UserInfoItem("Interested in",
+				R.drawable.ic_user_interest));
+		userInfoItems.add(new UserInfoItem("Profession",
+				R.drawable.ic_user_passion));
+		HashMap<UserInfoItem, UserDetailItem> userDetailItems = new HashMap<UserInfoItem, UserDetailItem>();
+		Boolean[] passion = { false, false };
+		Boolean[] profession = { false, false };
+		UserDetailItem item = new UserDetailItem(true, 1, passion, profession);
+		
+		UserInfoExpandListAdapter adapter = new UserInfoExpandListAdapter(
+				getActivity(), userInfoItems, item);
+		userList.setAdapter(adapter);
+		
+		return rootView;
+	}
 }
--- OnlineDating/src/online/dating/onlinedating/model/UserDetailItem.java
+++ OnlineDating/src/online/dating/onlinedating/model/UserDetailItem.java
@@ -1,5 +1,68 @@
 package online.dating.onlinedating.model;
 
 public class UserDetailItem {
+	Boolean isMale;
+	int height; /*
+				 * if height = 0 value low , height= 1 value medium and height =
+				 * 2 value long
+				 */
+	Boolean[] isPassionate;
+	Boolean[] isProfession;
+
+	public UserDetailItem(Boolean[] isProfession) {
+		super();
+		this.isProfession = isProfession;
+	}
+
+	public UserDetailItem(int height) {
+		super();
+		this.height = height;
+	}
+
+	public UserDetailItem(Boolean isMale) {
+		super();
+		this.isMale = isMale;
+	}
+
+	public UserDetailItem(Boolean isMale, int height, Boolean[] isPassionate,
+			Boolean[] isProfession) {
+		super();
+		this.isMale = isMale;
+		this.height = height;
+		this.isPassionate = isPassionate;
+		this.isProfession = isProfession;
+	}
+
+	public Boolean getIsMale() {
+		return isMale;
+	}
+
+	public void setIsMale(Boolean isMale) {
+		this.isMale = isMale;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Boolean[] getIsPassionate() {
+		return isPassionate;
+	}
+
+	public void setIsPassionate(Boolean[] isPassionate) {
+		this.isPassionate = isPassionate;
+	}
+
+	public Boolean[] getIsProfession() {
+		return isProfession;
+	}
+
+	public void setIsProfession(Boolean[] isProfession) {
+		this.isProfession = isProfession;
+	}
 
 }
