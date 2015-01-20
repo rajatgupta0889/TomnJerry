16b6eb9e008ba810023fc321bd857c8487e4d4c3
Rajat Gupta <rajatgupta0889@gmail.com>
2015-01-20 11:14:54 +0530

    update

---
+++ OnlineDating/src/online/daing/onlinedating/PagesFragment.java
@@ -0,0 +1,29 @@
+package online.daing.onlinedating;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagesFragment extends Fragment {
+	
+
+	public PagesFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.settings, container, false);
+		return rootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+
+	}
+
+}
